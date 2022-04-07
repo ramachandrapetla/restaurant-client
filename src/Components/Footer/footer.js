3d8dd756f4749d3f94774c0433d36ca8bbfa501a
ramachandrapetla <ramachandra1996@gmail.com>
2022-04-07 00:26:05 -0500

    initial changes

---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,14 @@
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <section id={"footer-section"}>
+            <div className = "footer-container">
+                <h1>Bale Bojanam</h1>
+                <p>&copy; Created and maintained by Ramachandra Petla</p>
+            </div>
+        </section>
+    )
+}
+
+export {Footer};
